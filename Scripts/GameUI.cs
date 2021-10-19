b6e02378a2f9f7a90b7862633da7cbbac26f3588
DeonMartinez <49213704+DeonMartinez@users.noreply.github.com>
2021-10-19 01:31:42 -0700

    Add files via upload

---
+++ Scripts/GameUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class GameUI : MonoBehaviour
+{
+    public TextMeshProUGUI goldText;
+    // instance
+    public static GameUI instance;
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public void UpdateGoldText(int gold)
+    {
+        goldText.text = "<b>Gold:</b> " + gold;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
